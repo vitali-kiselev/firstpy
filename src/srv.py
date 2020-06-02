3936bb0d15dc65c6a6b9f5a36a7c1ea7eb5c09b8
Виталий Киселев <v>
2020-06-03 00:01:06 +0300

    for herocu

--- src/srv.py
+++ src/srv.py
@@ -10,19 +10,19 @@ print(f"{PORT}")
 class MyHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path.startswith("/hello"):
-            path, qs = self.parth.split("?")
-            qs = parse_qs(qs)
-            mane = qs["name"][0]
-            msg = f"Hello {name}   Your path: {path}"
+          path, qs = self.parth.split("?")
+          qs = parse_qs(qs)
+          mane = qs["name"][0]
+          msg = f"Hello {name}   Your path: {path}"
 
           self.send_response(200)
           self.send_header("Content-type", "text/plain")
           self.send_header("Content-length", str(len(msg)))
           self.end_headers()
 
-        self.wfile.write(msg.encode())
+          self.wfile.write(msg.encode())
         else:
-            return SimpleHTTPRequestHandler.do_GET(self)
+          return SimpleHTTPRequestHandler.do_GET(self)
 
 
 
