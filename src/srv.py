a503babae98ce9c64cc07a569d545c526bdc1b6b
Виталий Киселев <v>
2020-06-05 00:15:55 +0300

    for herocu

--- src/srv.py
+++ src/srv.py
@@ -1,18 +1,18 @@
 import os
-import socketserver
+import socketserver #импорт модуля для создания сервера
 from http.server import SimpleHTTPRequestHandler
 from urllib.parse import parse_qs
 
-PORT = int(os.getenv("PORT", 8000))
-print(f"{PORT}")
+PORT = int(os.getenv("PORT", 8000)) #???
+print(f"port = {PORT}")
 
 
 class MyHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path.startswith("/hello"):
-          path, qs = self.parth.split("?")
+          path, qs = self.path.split("?")
           qs = parse_qs(qs)
-          mane = qs["name"][0]
+          name = qs["name"][0]
           msg = f"Hello {name}   Your path: {path}"
 
           self.send_response(200)
@@ -28,4 +28,4 @@ class MyHandler(SimpleHTTPRequestHandler):
 
 with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
     print("it works")
-    httpd.serve_forever()
+    httpd.serve_forever() #служить серверу вечно
