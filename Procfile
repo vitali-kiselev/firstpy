628ca9a35cb94074e63cc3e8b75d16a8495486c2
Виталий Киселев <v>
2020-06-10 00:42:10 +0300

    for herocu

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python -m http.server $PORT
+web: python -m src.srv
--- src/srv.py
+++ src/srv.py
@@ -1,31 +1,38 @@
 import os
-import socketserver #импорт модуля для создания сервера
+import socketserver #импортируем стандартную библиотеку для работы с сервером
 from http.server import SimpleHTTPRequestHandler
 from urllib.parse import parse_qs
 
-PORT = int(os.getenv("PORT", 8000)) #???
-print(f"port = {PORT}")
-
+PORT = int(os.getenv("PORT", 8000))
+print(f"PORT = {PORT}")
 
 class MyHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path.startswith("/hello"):
-          path, qs = self.path.split("?")
-          qs = parse_qs(qs)
-          name = qs["name"][0]
-          msg = f"Hello {name}   Your path: {path}"
-
-          self.send_response(200)
-          self.send_header("Content-type", "text/plain")
-          self.send_header("Content-length", str(len(msg)))
-          self.end_headers()
-
-          self.wfile.write(msg.encode())
+            return self.handle_hallo()
         else:
-          return SimpleHTTPRequestHandler.do_GET(self)
+            return SimpleHTTPRequestHandler(self)
+    def handle_hallo(self):
+        args = self.build_query_args()
+        name = args.get("name", "anonim")
+        self.respond(msg)
+    def build_query_args(self):
+        path, *qs = self.path.split("?")
+        args = {}
+        if len(qs) != 1:
+            return args
+            qs = parse_qs(qs)
+            name = qs["name"][0]
+
+    def respond(self,msg: str):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.send_header("Content-length", str(len(msg)))
+        self.end_headers()
 
 
+            
 
 with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
-    print("it works")
-    httpd.serve_forever() #служить серверу вечно
+    print("it" + " works")
+    httpd.serve_forever()
